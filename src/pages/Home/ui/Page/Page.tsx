ce402140892d7433d020a782ef4554ca4e1e56ef
Aleksey Popov <oluqom@proton.me>
2023-09-21 15:44:15 +0700

    feat: update Home page

--- src/pages/Home/ui/Page/Page.tsx
+++ src/pages/Home/ui/Page/Page.tsx
@@ -1,26 +1,22 @@
+import { Task } from "@/entities";
 import { FC } from "react";
 
 const Home: FC = () => {
   return (
     <>
       <section>
-        <div className="hero min-h-[calc(100vh-64px)] bg-base-200">
-          <div className="hero-content flex-col lg:flex-row">
-            <img
-              src="/images/hero.webp"
-              className="max-w-sm rounded-lg shadow-2xl"
+        <div className="container">
+          <div className="flex flex-col gap-y-3 py-5">
+            <Task
+              checked={true}
+              title={"Task"}
+              description={
+                "Praesent quis luctus risus. Nunc laoreet quam eu justo pharetra condimentum."
+              }
+              timeFrom={"21.09.23 - 10:00"}
+              timeTo={"21.09.23 - 21:00"}
+              tag={"yellow"}
             />
-            <div>
-              <h1 className="text-5xl font-bold">Welcome</h1>
-              <p className="py-5">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia
-                minima laboriosam maxime sed dignissimos harum provident itaque
-                fugiat. A repellat aliquid inventore dolor tempora, omnis
-                perferendis aspernatur quo nisi excepturi. Ex, ullam odio iusto
-                esse necessitatibus doloremque repudiandae!
-              </p>
-              <button className="btn-primary btn">Get Started</button>
-            </div>
           </div>
         </div>
       </section>
